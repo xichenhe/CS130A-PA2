06f2a32a1c9f6b17474ce0afca18d6ee3633fe13
xichenhe <xichenhe@ucsb.edu>
2022-03-06 16:38:34 -0800

    modified main

--- main.cpp
+++ main.cpp
@@ -1,8 +1,28 @@
 #include "GraphGenerator.h"
 #include "GraphOperator.h"
+#include <iostream>
+using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    GraphGenerator x;
+    x.readEdges("testedges.txt");
+    x.readHobbies("testhobbies.txt");
+
+    GraphOperator y(x.getVertices(),x.getHobbies(),x.getAdjacencyList(),x.getHobbiesList());
+
+    cout<<"The average degree:"<<endl;
+    cout<<y.FindAverageDegree()<<endl;
+
+    cout<<"The vertex with the highest degree:"<<endl;
+    cout<<y.FindHighestDegree()<<endl;
+
+    cout<<"The number of connected components:"<<endl;
+    cout<<y.FindConnectedNumber()<<endl;
+
+    
+    
+
+
     return 0;
 }
